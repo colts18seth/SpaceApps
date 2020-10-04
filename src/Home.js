d3f220c29c902d10f7b99fbfba2eb31342b3c57d
Seth <colts18seth@gmail.com>
2020-10-03 21:20:10 -0400

    initial

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <p>Citizen scientits: don't know where to begin?</p>
+            <p>We're here to help.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Sources from './Sources';
+
+function Routes() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route exact path='/about'>
+                    <About />
+                </Route>
+                <Route exact path='/sources'>
+                    <Sources />
+                </Route>
+                <Redirect to='/' />
+            </Switch>
+        </div>
+    );
+}
+
+export default Routes;
